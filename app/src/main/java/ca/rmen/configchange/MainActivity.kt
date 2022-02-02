fae17a2da42ae186cc6f17e0a24654311dfeae44
Carmen Alvarez <calvarez@libon.com>
2022-02-02 15:41:22 +0100

    simplify and clarify the names of the fragments/layouts

--- app/src/main/java/ca/rmen/configchange/MainActivity.kt
+++ app/src/main/java/ca/rmen/configchange/MainActivity.kt
@@ -5,25 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = Adapter()
-        recyclerView.adapter = adapter
+        findViewById<RecyclerView>(R.id.recycler_view).adapter = Adapter()
+    }
+
+    class ItemContentRed : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+            inflater.inflate(
+                R.layout.item_content_red,
+                container,
+                false
+            )
     }
 
     class Adapter : RecyclerView.Adapter<ItemViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_container_yellow, parent, false)
         )
 
-        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) { }
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {}
 
         override fun getItemCount() = 1
     }
